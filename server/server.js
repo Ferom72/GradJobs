94b44b2cb10a84069356166d91e8b127d2904e5b
Ferom72 <romerof0005@gmail.com>
2023-07-25 16:04:54 -0400

    changes

--- server/server.js
+++ server/server.js
@@ -14,5 +14,5 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use('/',require("./routes/authRoutes"))
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, ()=> console.log(`Server is running on port ${port}`))
